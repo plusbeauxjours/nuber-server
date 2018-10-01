48cc3f362dacd6af46d598cc8a2cef08a20dd747
plusbeauxjours <plusbeauxjours@gmail.com>
2018-10-01 09:41:39 +0200

    #1.11 Creating a Virtual Environment on NodeJS

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { Options } from "graphql-yoga";
 import { createConnection } from 'typeorm';
 import app from "./app";
@@ -18,3 +20,4 @@ const handleAppStart = () => console.log(`Listening on port ${PORT}`);
 createConnection(connectionOptions).then(() => {
     app.start(appOptions, handleAppStart);
 })
+.catch(error => console.log(error));
--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -8,9 +8,9 @@ const connectionOptions: ConnectionOptions = {
     entities: [
         "entities/**/*.*"
     ],
-    host: process.env.DB_ENDPOINT || 'localhost',
+    host: process.env.DB_ENDPOINT,
     port: 5432,
-    username: process.env.DB_USERNAME || 'plusbeauxjours',
-    password: process.env.DB_USERNAME || ''
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
 }
 export default connectionOptions;
