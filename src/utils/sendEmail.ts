6d2ec854f43f0609fa182954af6dfa4d602094b9
plusbeauxjours <plusbeauxjours@gmail.com>
2018-10-26 22:01:06 +0200

    #1.50 Sending Confirmation Email part Two

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,6 +1,22 @@
 import Mailgun from 'mailgun-js';
 
 const mailGunClient = new Mailgun({
-    apiKey: process.env.Mailgun_API_KEY || "",
+    apiKey: process.env.MAILGUN_API_KEY || "",
     domain: "sandboxf4ae731f542b44a786ee23bfab8dc714.mailgun.org"
 });
+
+const sendEmail = (subject: string, html: string) => {
+    const emailData: Mailgun.messages.SendData = {
+        from "plusbeauxjours@gmail.com",
+        to: "plusbeauxjours@gmail.com",
+        subject,
+        html
+    }
+    return mailGunClient.messages().send(emailData)
+}
+
+export const sendVerificat ionEmail = (fullName: string, key: string) => {
+    const emailSubject = `Hello! ${fullName}, please verify your email`;
+    const emailBody = `Verify your email by clicking <a href="http://plusbeauxjours.com/${key}/">here</a>`;
+    return sendEmail(emailSubject, emailBody);
+}
