8bc0de4d8adf2b05d61cc38d856407918f6c5f0a
plusbeauxjours <plusbeauxjours@gmail.com>
2018-12-18 19:20:39 +0700

    Bugfix

--- src/api/Chat/GetChat/GetChat.resolvers.ts
+++ src/api/Chat/GetChat/GetChat.resolvers.ts
@@ -5,47 +5,49 @@ import { Resolvers } from "../../../types/resolvers";
 import privateResolver from "../../../utils/privateResolver";
 
 const resolvers: Resolvers = {
-    Query: {
-        GetChat: privateResolver(
-            async (_, args: GetChatQueryArgs, { req }): Promise<GetChatResponse> => {
-                const user: User = req.user;
-                await User.update({id: 1}, {isRiding: false, isTaken: false});
-                try {
-                    const chat = await Chat.findOne({
-                        id: args.chatId
-                    }, 
-                    { relations: ["passenger", "driver"] });
-                    if (chat) {
-                        if (chat.passengerId === user.id || chat.driverId === user.id) {
-                            return {
-                                ok: true,
-                                error: null,
-                                chat
-                            };
-                        } else {
-                            return {
-                                ok: false,
-                                error: "Not authorized to see this chat",
-                                chat: null
-                            };
-                        }
-                    } else {
-                        return {
-                            ok: false,
-                            error: "Not found",
-                            chat: null
-                        };
-                    }
-                } catch (error) {
-                    return {
-                        ok: false,
-                        error: error.message,
-                        chat: null
-                    };
-                }
+  Query: {
+    GetChat: privateResolver(
+      async (_, args: GetChatQueryArgs, { req }): Promise<GetChatResponse> => {
+        const user: User = req.user;
+        await User.update({ id: 1 }, { isRiding: false, isTaken: false });
+        try {
+          const chat = await Chat.findOne(
+            {
+              id: args.chatId
+            },
+            { relations: ["passenger", "driver"] }
+          );
+          if (chat) {
+            if (chat.passengerId === user.id || chat.driverId === user.id) {
+              return {
+                ok: true,
+                error: null,
+                chat
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Not authorized to see this chat",
+                chat: null
+              };
             }
-        )
-    }
+          } else {
+            return {
+              ok: false,
+              error: "Not found",
+              chat: null
+            };
+          }
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            chat: null
+          };
+        }
+      }
+    )
+  }
 };
 
 export default resolvers;
--- src/entities/Message.ts
+++ src/entities/Message.ts
@@ -1,35 +1,38 @@
 import {
-    BaseEntity,
-    Column,
-    CreateDateColumn,
-    Entity,
-    ManyToOne,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn,
-} from 'typeorm';
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
 
-import Chat from './Chat';
+import Chat from "./Chat";
 import User from "./User";
 
 @Entity()
 class Message extends BaseEntity {
-    @PrimaryGeneratedColumn() id: number
+  @PrimaryGeneratedColumn() id: number;
 
-    @Column({type: 'text'})
-    text: string;
-    
-    @Column({ nullable: true })
-    chatId: number;
+  @Column({ type: "text" })
+  text: string;
 
-    @ManyToOne(type => Chat, chat => chat.messages)
-    chat: Chat;
+  @Column({ nullable: true })
+  chatId: number;
 
-    @ManyToOne(type => User, user => user.messages)
-    user: User;
+  @ManyToOne(type => Chat, chat => chat.messages)
+  chat: Chat;
 
-    @CreateDateColumn() createdAt: string;
+  @Column({ nullable: true })
+  userId: number;
 
-    @UpdateDateColumn() updatedAt: string;
+  @ManyToOne(type => User, user => user.messages)
+  user: User;
+
+  @CreateDateColumn() createdAt: string;
+
+  @UpdateDateColumn() updatedAt: string;
 }
 
 export default Message;
